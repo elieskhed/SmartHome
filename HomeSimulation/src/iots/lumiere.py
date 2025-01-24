b83b7ade66d6060a5472e3673377e687178ae388
Paul Marcelet <paul@MacBook-Pro-de-Paul.local>
2025-01-24 11:07:10 +0100

    on to 1

--- HomeSimulation/src/iots/lumiere.py
+++ HomeSimulation/src/iots/lumiere.py
@@ -5,10 +5,12 @@ import random
 class Light(Device):
     def __init__(self, device_id, device_name, device_location):
         super().__init__(device_id, device_name, device_location, 'device/light')
-        self.device_value = "Off"
+        #self.device_value = "Off"
+        self.device_value = 0
         
     def setRandomValue(self):
-        self.device_value = random.choice(["On", "Off"])
+        #self.device_value = random.choice(["On", "Off"])
+        self.device_value = random.choice([1, 0])
 
     def convertDataToJSON(self):
         data = {
