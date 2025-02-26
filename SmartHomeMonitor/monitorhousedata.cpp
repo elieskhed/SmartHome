4fffc539d9f8bc8ecf98c5c75ac899b229043e0e
elies <elieshanachi@gmail.com>
2025-02-26 12:52:39 +0100

    Ajout onglets2 SmartHomeMonitpr ==>Graphes temps réel à venir

--- SmartHomeMonitor/monitorhousedata.cpp
+++ SmartHomeMonitor/monitorhousedata.cpp
@@ -1,3 +1,17 @@
 #include "monitorhousedata.h"
 
-MonitorHouseData::MonitorHouseData() {}
+MonitorHouseData::MonitorHouseData() {
+    QLabel* labelHouseToken = new QLabel("Token de la maison");
+    this->houseToken = new QTextEdit();
+    this->houseToken->setFixedSize(750, 50);
+
+    this->buttonSend = new QPushButton("Analyser la donnée");
+
+    this->verticalLayout = new QVBoxLayout();
+    this->verticalLayout->addWidget(labelHouseToken);
+    this->verticalLayout->addWidget(this->houseToken);
+    this->verticalLayout->addWidget(this->buttonSend);
+
+    this->setLayout(this->verticalLayout);
+
+}
--- SmartHomeMonitor/monitorhousedata.h
+++ SmartHomeMonitor/monitorhousedata.h
@@ -2,11 +2,23 @@
 #define MONITORHOUSEDATA_H
 
 #include <QWidget>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QVBoxLayout>
 
 class MonitorHouseData : public QWidget
 {
+    Q_OBJECT
 public:
     MonitorHouseData();
+
+private:
+    QTextEdit* houseToken;
+    QPushButton* buttonSend;
+
+    QVBoxLayout* verticalLayout;
+
 };
 
 #endif // MONITORHOUSEDATA_H
