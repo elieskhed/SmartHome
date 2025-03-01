8de815b7c36ce34d67c6351ebe73eb5a9e220bb6
elies <elieshanachi@gmail.com>
2025-03-01 14:57:10 +0100

    JSONParser avancement

--- SmartHomeMonitor/apiresponseparser.cpp
+++ SmartHomeMonitor/apiresponseparser.cpp
@@ -1,3 +1,24 @@
 #include "apiresponseparser.h"
 
-ApiResponseParser::ApiResponseParser() {}
+
+ApiResponseParser::ApiResponseParser(QString const& jsonStr): jsonStr(jsonStr) {
+
+    // Conversion de la chaine en QByteArray
+    QJsonDocument doc = QJsonDocument::fromJson(this->jsonStr.toUtf8());
+
+    // Verification que le document contient un objet JSON
+    if (!doc.isObject()){
+        qWarning() << "Le JSON n'est pas un objet valide";
+    }
+
+    // On récupere l'objet JSON
+    //this->jsonObj = new QJsonObject();
+    this->jsonObj = doc.object();
+
+    // Extraction des valeurs
+
+}
+
+void ApiResponseParser::parseLastData(){
+
+}
--- SmartHomeMonitor/apiresponseparser.h
+++ SmartHomeMonitor/apiresponseparser.h
@@ -1,10 +1,27 @@
 #ifndef APIRESPONSEPARSER_H
 #define APIRESPONSEPARSER_H
 
-class ApiResponseParser
+#include <QObject>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QString>
+#include <QStringList>
+#include <QJsonDocument>
+#include <QDebug>
+
+
+class ApiResponseParser : public QObject
 {
+    Q_OBJECT
 public:
-    ApiResponseParser();
+    ApiResponseParser(QString const& jsonStr);
+
+    void parseLastData();
+
+
+private:
+    QString jsonStr;
+    QJsonObject jsonObj;
 };
 
 #endif // APIRESPONSEPARSER_H
