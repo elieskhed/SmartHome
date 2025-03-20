d32f47a7f77a77d94eaaeaec90cfc8fd6c82a10c
elies <elieshanachi@gmail.com>
2025-03-20 13:52:51 +0100

    Tests API fonctionnelles

--- HomeSimulation/src/iots/main.py
+++ HomeSimulation/src/iots/main.py
@@ -8,14 +8,17 @@ BROKER = '172.20.10.5'
 # PORT   = 1883
 PORT = 8883
 
+# Auhentification de la maison, et association de celle-ci avec
+# Les informations du Propriétaire récupéré depuis SmartHomeAPI.
 ca_cert     = './certs/CA/ca.crt'
 client_cert = './certs/clients/client.crt'
 client_key  = './certs/clients/client.key'
-house_token = "Wdp4HCPNvC3wHnvSfSwmVknhLuHtjUkoIzG6zPno"
+house_token = "1CMaPGPNA0ZH8wwkWHcHrdQtHpv1fipAnQolMLYL"
+house_name = "housename"
 
 devices = [
-    Light("eliesHouse", house_token,1, 'lamp_kit', 'kitchen', 0),
-    HumiditySensor("eliesHouse", house_token,1, 'humid_sens', 'kitchen', 0.12)
+    Light(house_name, house_token,1, 'lamp_kit', 'kitchen', 0),
+    HumiditySensor(house_name, house_token,1, 'humid_sens', 'kitchen', 0.12)
 ]
 
 house = House(devices, BROKER, PORT)
